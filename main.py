7f57c5fe236090c62056bde0770e578cda9e0f32
YuScriptX <yusuf.tiftik@outlook.com>
2025-10-31 10:21:50 +0100

    implement age category classification program

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Age category
 
+age = int(input("Enter your age?: "))
+
+def age_cat():
+    if age < 13:
+        return "Child"
+    elif age >= 13 and age < 19:
+        return "Teenager"
+    else:
+        return "Adult"
+
+print(age_cat())
